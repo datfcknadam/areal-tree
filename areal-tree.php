d6d648ad6f694b15de67e561fe7fb5107d3ec8ce
Kulikov Egor <benstiller68@gmail.com>
2019-10-10 12:34:58 +0300

    refactor

--- areal-tree.php
+++ areal-tree.php
@@ -7,12 +7,15 @@ class Tree {
     $size = filesize($path);
     $units = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
     $power = $size > 0 ? floor(log($size, 1024)) : 0;
+
     return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
   }
+
   public function __construct($is_dir, $is_arg) {
       $this->is_dir = $is_dir;
       $this->is_arg = $is_arg;
   }
+
   function outputTree ($dir, $iterator, $search_file) {
     $iterator .= "   ";
     $handle = opendir($dir);
@@ -22,37 +25,31 @@ class Tree {
       if ($file == '.' || $file == '..') {
         continue;
       }
-      else {
-        if (is_file($dir.DIRECTORY_SEPARATOR.$file) && $search_file) {
-          echo $iterator."└──".$file."(".$this->filesize_formatted($dir.DIRECTORY_SEPARATOR.$file).")\n";
-        }
-        if (is_dir(($dir.DIRECTORY_SEPARATOR.$file))) {
-          echo $iterator."└──".$file."\n";
-          $this->outputTree($dir.DIRECTORY_SEPARATOR.$file, $iterator, $search_file);
-        }
+      if (is_file($dir.DIRECTORY_SEPARATOR.$file) && $search_file) {
+        echo $iterator."└──".$file."(".$this->filesize_formatted($dir.DIRECTORY_SEPARATOR.$file).")\n";
       }
-
+      if (is_dir($dir.DIRECTORY_SEPARATOR.$file)) {
+        echo $iterator."└──".$file."\n";
+        $this->outputTree($dir.DIRECTORY_SEPARATOR.$file, $iterator, $search_file);
+      }
     }
     closedir($handle);
   }
+
   public function output() {
-    if (isset($this->is_dir)) {
+
+    try {
+      if ($this->is_arg && $this->is_arg !== "-f") {
+        throw new Exception("Error: undefined argument '".$this->is_arg."'\n");
+      }
       chdir($this->is_dir);
       $dir = getcwd();
-      $iterator = "|";
+      $iterator = "";
+      $this->outputTree($dir, $iterator, $this->is_arg);
 
-      if(empty($this->is_arg)) {
-        $this->outputTree($dir, $iterator, false);
-      }
-      elseif ($this->is_arg === "-f") {
-        $this->outputTree($dir, $iterator, true);
-      }
-      else {
-        echo "Error: undefined argument '".$this->is_arg."'\n";
-      }
     }
-    else {
-      echo "Error: please pass the directory in first argument \n";
+    catch (Exception $e) {
+      echo $e->getMessage();
     }
   }
 }
--- run.php
+++ run.php
@@ -1,6 +1,22 @@
-﻿
-<?php 
+﻿<?php
   require_once "areal-tree.php";
-  $tree = new Tree($argv[1], $argv[2]);
-  echo $tree->output();
+
+  try {
+    if (empty($argv[1])) {
+      throw new Exception("Error: please pass the directory in first argument. \n");
+    }
+    if (!is_dir($argv[1])) {
+      throw new Exception("Error: such directory not found.\n");
+    }
+    if(isset($argv[2])) {
+      $tree = new Tree($argv[1], $argv[2]);
+    }
+    else {
+      $tree = new Tree($argv[1], null);
+    }
+    echo $tree->output();
+  }
+  catch(Exception $e) {
+    echo $e->getMessage();
+  }
 ?>
